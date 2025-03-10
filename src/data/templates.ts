81cc070f2d48ffa869ac1a3dcf32ad8d12d5c7fb
Shin-sibainu <shincode0712@gmail.com>
2025-03-10 17:24:11 +0900

    not available classic

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -49,7 +49,8 @@ export const templates: Template[] = [
       "検索機能",
     ],
     isNew: true,
-    available: true,
+    // available: true,
+    available: false,
     demoUrl: "https://classic.notepress.xyz",
   },
 ];
